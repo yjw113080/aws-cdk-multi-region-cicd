f6f54b1b40668947a2861f32db535fc9738016ca
Yeom <jiwony@a483e7428246.ant.amazon.com>
2020-03-26 22:21:55 +0900

    intial commit

--- bin/multi-cluster-cicd.ts
+++ bin/multi-cluster-cicd.ts
@@ -4,4 +4,7 @@ import * as cdk from '@aws-cdk/core';
 import { MultiClusterCicdStack } from '../lib/multi-cluster-cicd-stack';
 
 const app = new cdk.App();
-new MultiClusterCicdStack(app, 'MultiClusterCicdStack');
+new MultiClusterCicdStack(app, 'MultiClusterCicdStack', {
+    env: { account: app.node.tryGetContext('account') || process.env.CDK_INTEG_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT, 
+           region: 'ap-northeast-1' },
+});
--- lib/multi-cluster-cicd-stack.ts
+++ lib/multi-cluster-cicd-stack.ts
@@ -1,9 +1,64 @@
 import * as cdk from '@aws-cdk/core';
+import codecommit = require('@aws-cdk/aws-codecommit');
+import ecr = require('@aws-cdk/aws-ecr');
+import codebuild = require('@aws-cdk/aws-codebuild');
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import pipelineAction = require('@aws-cdk/aws-codepipeline-actions');
+import * as iam from '@aws-cdk/aws-iam';
 
 export class MultiClusterCicdStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const repoForCDK = new codecommit.Repository(this, 'repo-for-cdk', {
+      repositoryName: 'repo-for-cdk'
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const buildForCDK = new codebuild.PipelineProject(this, 'build-for-cdk', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.UBUNTU_14_04_NODEJS_10_14_1
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: "0.2",
+        phases: {
+            pre_build: {
+                commands: [ `npm i -g aws-cdk`, `npm i` ]
+            },
+            build: {
+              commands: [
+                `arr=$(cdk list)`,
+                'for i in "${arr[@]}"; do cdk deploy $i --require-approval never; done'
+              ]
+            }
+          }
+      })
+    });
+    buildForCDK.addToRolePolicy(new iam.PolicyStatement({
+      actions: [`*`],
+      resources: [`*`]
+    }));
+
+    const pipelineForCDK = new codepipeline.Pipeline(this, 'pipeline-for-cdk', {
+      stages: [
+        {
+          stageName: 'source',
+          actions: [ new pipelineAction.CodeCommitSourceAction({
+            actionName: 'catch-the-cdk-code',
+            repository: repoForCDK,
+            output: sourceOutput
+          },
+        ) ]
+        },{
+          stageName: 'build-and-deploy',
+          actions: [ new pipelineAction.CodeBuildAction({
+            actionName: 'build-and-deploy-cdk',
+            input: sourceOutput,
+            project: buildForCDK
+          }) ]
+        }
+      ]
+    })
+
   }
 }
