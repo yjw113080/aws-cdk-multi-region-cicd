6a76b7234e9b99e38d05e302b09e3d5e34a4b55f
Yeom <jiwony@a483e7428246.ant.amazon.com>
2020-05-04 13:12:07 +0900

    cdk deploy fixed

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-# Welcome to your CDK TypeScript project!
 
-This is a blank project for TypeScript development with CDK.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+# CI/CD Pipeline for CDK deployment
 
-## Useful commands
+이 레포지토리의 코드는,  
+1) CodeCommit 2) CodeBuild 3) CodePipeline을 이용하여 여러분이 인프라를 정의한 CDK 코드를 자동 배포해주는 파이프라인을 생성합니다.
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+
+
--- lib/multi-cluster-cicd-stack.ts
+++ lib/multi-cluster-cicd-stack.ts
@@ -19,9 +19,7 @@ export class MultiClusterCicdStack extends cdk.Stack {
     });
     roleToAssume.addToPolicy(new iam.PolicyStatement({ actions: [`*`], resources: [`*`]}));
 
-
-    const sourceOutput = new codepipeline.Artifact();
-    const buildForCDK = new codebuild.PipelineProject(this, 'build-for-cdk', {
+    const deployCDK = new codebuild.PipelineProject(this, 'build-for-cdk', {
       environment: {
         buildImage: codebuild.LinuxBuildImage.UBUNTU_14_04_NODEJS_10_14_1,
         privileged: true
@@ -30,47 +28,47 @@ export class MultiClusterCicdStack extends cdk.Stack {
         version: "0.2",
         phases: {
             pre_build: {
-                commands: [ `npm i -g aws-cdk`, `npm i`, `npm run-script build` ]
+              commands: [`npm i -g aws-cdk`, `npm i`, `npm run-script build`]
             },
             build: {
               commands: [
-
                 `CREDENTIALS=$(aws sts assume-role --role-arn "${roleToAssume.roleArn}" --role-session-name codebuild-cdk)`,
                 `export AWS_ACCESS_KEY_ID="$(echo \${CREDENTIALS} | jq -r '.Credentials.AccessKeyId')"`,
                 `export AWS_SECRET_ACCESS_KEY="$(echo \${CREDENTIALS} | jq -r '.Credentials.SecretAccessKey')"`,
                 `export AWS_SESSION_TOKEN="$(echo \${CREDENTIALS} | jq -r '.Credentials.SessionToken')"`,
                 `export AWS_EXPIRATION=$(echo \${CREDENTIALS} | jq -r '.Credentials.Expiration')`,
-
-                `for i in "$(cdk list)"; do cdk deploy $i --require-approval never; done`
-
+                `cdk deploy '*' --require-approval never; done`
               ]
             }
           }
       })
     });
 
-    buildForCDK.addToRolePolicy(new iam.PolicyStatement({
+    deployCDK.addToRolePolicy(new iam.PolicyStatement({
       actions: [`sts:AssumeRole`],
       resources: [roleToAssume.roleArn]
     }));
     
+    const sourceOutput = new codepipeline.Artifact();
+
 
     const pipelineForCDK = new codepipeline.Pipeline(this, 'pipeline-for-cdk', {
       stages: [
         {
-          stageName: 'source',
+          stageName: 'Source',
           actions: [ new pipelineAction.CodeCommitSourceAction({
             actionName: 'catch-the-cdk-code',
             repository: repoForCDK,
             output: sourceOutput
           },
         ) ]
-        },{
-          stageName: 'build-and-deploy',
+        },
+        {
+          stageName: 'Deploy',
           actions: [ new pipelineAction.CodeBuildAction({
-            actionName: 'build-and-deploy-cdk',
+            actionName: 'deploy-CDK',
             input: sourceOutput,
-            project: buildForCDK
+            project: deployCDK
           }) ]
         }
       ]
